ec7e2ebe97c9459890d22dc069b5df41537d2109
minodaperera <minodavilaniperera@gmail.com>
2026-04-20 22:57:46 +0530

    feat: add Resource repository

---
+++ backend/src/main/java/com/smartcampus/repository/ResourceRepository.java
@@ -0,0 +1,15 @@
+package com.smartcampus.repository;
+
+import com.smartcampus.entity.Resource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
+
+    List<Resource> findByType(String type);
+
+    List<Resource> findByLocationContainingIgnoreCase(String location);
+
+    List<Resource> findByCapacityGreaterThanEqual(Integer capacity);
+}
