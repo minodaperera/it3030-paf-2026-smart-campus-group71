f95bb20c11f47333703f8d9bfabbfb5525149d4a
minodaperera <minodavilaniperera@gmail.com>
2026-04-20 22:58:13 +0530

    feat: add custom ResourceNotFound exception

---
+++ backend/src/main/java/com/smartcampus/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.smartcampus.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(Long id) {
+        super("Resource not found with id: " + id);
+    }
+}
